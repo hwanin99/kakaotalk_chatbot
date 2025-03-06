d225db34457f97ecdfe676af7e9967dd0fd48ee6
Kim Sung Hwan <tjdghks0521@naver.com>
2025-03-06 21:33:04 +0900

    Update and rename kakao_chatbot.py to asynchronous.py

---
+++ asynchronous.py
@@ -0,0 +1,20 @@
+
+# 응답시간 초과시 먼저 답변
+def timeover():
+    response = {'version': '2.0', 'template': {
+        'outputs': [
+            {
+                'simpleText': {
+                    'text': "앗 죄송합니다! 아직 생각 중이에요.\n잠시 후 아래 말풍선을 눌러주세요."
+                }
+            }
+        ], 'quickReplies': [
+            # 생각이 다 끝남? 버튼생성
+            {
+                'action': 'message',
+                'label': '생각 다 했니?',
+                'messageText': '생각 다 했니?'
+            }
+        ]
+    }}
+    return response
