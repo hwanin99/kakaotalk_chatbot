0ff36c20c22860abb7a2863951616a0fdc688ff8
Kim Sung Hwan <tjdghks0521@naver.com>
2025-03-06 21:41:17 +0900

    Update kakao_chatbot.py

--- kakao_chatbot.py
+++ kakao_chatbot.py
@@ -1,118 +1,9 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# # Import Module
-# ---
-# >* openai == 0.28.1
-# >* translate == 3.6.1
-
-# In[ ]:
-
-
-import json
-import openai
-import threading
-import time
-import queue as q
 import os
-from translate import Translator
+import time
+import json
+import threading
+import queue as q
 
-# OpenAI API KEY
-openai.api_key = os.environ['OPENAI_API']
-
-
-# In[ ]:
-
-
-
-
-
-# # ChatGPT & DALLE-2 사용하기
-# ---
-# >* prompt를 받아서 ChatGPT/DALLE-2가 응답해주는 함수
-#     * DALLE-2는 한국말을 이해를 잘 못하기에 Translator를 통해 한국말을 prompt로 입력하면 자동으로 영어로 번역해서 입력해주는 기능을 추가  
-#     
-# >* ChatGPT/DALLE-2에게 응답받은 값을 카카오톡 챗봇 형식에 맞게 json형식으로 변환
-
-# In[ ]:
-
-
-# ChatGPT에게 질문/답변 받기
-def getTextFromGPT(prompt):
-    messages_prompt = [{"role": "system", "content": 'You are a thoughtful assistant. Respond to all input in 25 words and answer in korea'}]
-    messages_prompt += [{"role": "user", "content": prompt}]
-    response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=messages_prompt)
-    message = response["choices"][0]["message"]["content"]
-    return message
-
-# DALLE-2에게 질문/그림 URL 받기
-def getImageURLFromDALLE(prompt):
-    translator = Translator(from_lang='ko',to_lang='en')
-    prompt = translator.translate(prompt) # prompt를 영어로 번역
-    response = openai.Image.create(prompt=prompt,n=1,size="512x512")
-    image_url = response['data'][0]['url']
-    return image_url
-
-# 카카오채널에 gpt의 메세지 전송
-def textResponseFormat(bot_response):
-    response = {'version': '2.0', 'template': {
-        'outputs': [{'simpleText': {'text': bot_response}}], 'quickReplies': []}}
-    return response
-
-# 카카오채널에 dalle 이미지 전송
-def imageResponseFormat(bot_response, prompt):
-    output_text = prompt + "내용에 관한 이미지 입니다"
-    response = {'version': '2.0', 'template': {
-        'outputs': [{'simpleImage': {'imageUrl': bot_response, 'altText': output_text}}], 'quickReplies': []}}
-    return response
-
-
-# In[ ]:
-
-
-
-
-
-# # 비동기 처리
-# ---
-# >* 카카오톡 챗봇의 경우에는 5초간 응답이 없으면 에러가 발생
-#     * DALLE-2는 응답이 5초가 넘는 경우가 대부분이기에, 이를 해결하기 위해 비동기 처리
-
-# In[ ]:
-
-
-# 응답시간 초과시 먼저 답변
-def timeover():
-    response = {'version': '2.0', 'template': {
-        'outputs': [
-            {
-                'simpleText': {
-                    'text': "앗 죄송합니다! 아직 생각 중이에요.\n잠시 후 아래 말풍선을 눌러주세요."
-                }
-            }
-        ], 'quickReplies': [
-            # 생각이 다 끝남? 버튼생성
-            {
-                'action': 'message',
-                'label': '생각 다 했니?',
-                'messageText': '생각 다 했니?'
-            }
-        ]
-    }}
-    return response
-
-
-# In[ ]:
-
-
-
-
-
-# # Main
-# ---
-# >* aws lambda를 사용하기 위해 아래와 같이 메인 함수를 사용 
-
-# In[ ]:
 
 
 # 메인 함수
@@ -212,4 +103,3 @@ def responseOpenAI(request,response_queue,filename):
 def dbReset(filename):
     with open(filename, 'w') as f:
         f.write("")
-
